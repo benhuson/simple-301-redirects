f9ca5abd1cbe9697e21e271f309b2459f695046e
scottnelle <scottnelle@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-31 04:28:49 +0000

    Added wildcard support. This is very much a development version, but it works in my testing.
    git-svn-id: https://plugins.svn.wordpress.org/simple-301-redirects/trunk@661502 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-simple-301-redirects.php
+++ wp-simple-301-redirects.php
@@ -2,13 +2,13 @@
 /*
 Plugin Name: Simple 301 Redirects
 Plugin URI: http://www.scottnelle.com/simple-301-redirects-plugin-for-wordpress/
-Description: Create a list of URLs that you would like to 301 redirect to another page or site
-Version: 1.04a1
+Description: Create a list of URLs that you would like to 301 redirect to another page or site. This version is not for public release.
+Version: 1.04a2
 Author: Scott Nellé
 Author URI: http://www.scottnelle.com/
 */
 
-/*  Copyright 2009  Scott Nellé  (email : contact@scottnelle.com)
+/*  Copyright 2009-2013  Scott Nellé  (email : contact@scottnelle.com)
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -26,62 +26,92 @@ Author URI: http://www.scottnelle.com/
 */
 
 if (!class_exists("Simple301redirects")) {
+	
 	class Simple301Redirects {
-		/*
-			generate the link to the options page under settings
-		*/
+		
+		/**
+		 * create_menu function
+		 * generate the link to the options page under settings
+		 * @access public
+		 * @return void
+		 */
 		function create_menu() {
 		  add_options_page('301 Redirects', '301 Redirects', 'manage_options', '301options', array($this,'options_page'));
 		}
 		
-		/*
-			generate the options page in the wordpress admin
-		*/
+		/**
+		 * options_page function
+		 * generate the options page in the wordpress admin
+		 * @access public
+		 * @return void
+		 */
 		function options_page() {
 		?>
 		<div class="wrap">
+		<script>
+			// Do this the right way before public release!
+			jQuery(document).ready(function(){
+				jQuery('span.wps301-delete').html('Delete').css({'color':'red','cursor':'pointer'}).click(function(){
+					var confirm_delete = confirm('Delete This Redirect?');
+					if (confirm_delete) {
+						jQuery(this).parent().parent().remove();
+					}
+				});
+			});
+		</script>
 		<h2>Simple 301 Redirects</h2>
 		
 		<form method="post" action="options-general.php?page=301options">
 		
-		<table>
-			<tr>
-				<th>Request</th>
-				<th>Destination</th>
-			</tr>
-			<tr>
-				<td><small>example: /about.htm</small></td>
-				<td><small>example: <?php echo get_option('home'); ?>/about/</small></td>
-			</tr>
-			<tr>
-				<td><input type="text" name="301_redirects[request][]" value="" style="width:15em" />&nbsp;&raquo;&nbsp;</td>
-				<td><input type="text" name="301_redirects[destination][]" value="" style="width:30em;" /></td>
-			</tr>
-			<?php echo $this->expand_redirects(); ?>
+		<table class="widefat">
+			<thead>
+				<tr>
+					<th colspan="2">Request</th>
+					<th colspan="2">Destination</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td colspan="2"><small>example: /about.htm</small></td>
+					<td colspan="2"><small>example: <?php echo get_option('home'); ?>/about/</small></td>
+				</tr>
+				<?php echo $this->expand_redirects(); ?>
+				<tr>
+					<td style="width:35%;"><input type="text" name="301_redirects[request][]" value="" style="width:99%;" /></td>
+					<td style="width:2%;">&raquo;</td>
+					<td style="width:60%;"><input type="text" name="301_redirects[destination][]" value="" style="width:99%;" /></td>
+					<td><span class="wps301-delete">Delete</span></td>
+				</tr>
+			</tbody>
 		</table>
 		
-		<p class="submit">
-		<input type="submit" name="submit_301" class="button-primary" value="<?php _e('Save Changes') ?>" />
-		</p>
+		<?php $wildcard_checked = (get_option('301_redirects_wildcard') === 'true' ? ' checked="checked"' : ''); ?>
+		<p><input type="checkbox" name="301_redirects[wildcard]" id="wps301-wildcard"<?php echo $wildcard_checked; ?> /><label for="wps301-wildcard"> Use Wildcards?</label></p>
+		
+		<p class="submit"><input type="submit" name="submit_301" class="button-primary" value="<?php _e('Save Changes') ?>" /></p>
 		</form>
 		</div>
 		<?php
 		} // end of function options_page
 		
-		/*
-			utility function to return the current list of redirects as form fields
-		*/
+		/**
+		 * expand_redirects function
+		 * utility function to return the current list of redirects as form fields
+		 * @access public
+		 * @return string <html>
+		 */
 		function expand_redirects() {
 			$redirects = get_option('301_redirects');
 			$output = '';
 			if (!empty($redirects)) {
-				$redirects = array_reverse($redirects);
 				foreach ($redirects as $request => $destination) {
 					$output .= '
 					
 					<tr>
-						<td><input type="text" name="301_redirects[request][]" value="'.$request.'" style="width:15em" />&nbsp;&raquo;&nbsp;</td>
-						<td><input type="text" name="301_redirects[destination][]" value="'.$destination.'" style="width:30em;" /></td>
+						<td><input type="text" name="301_redirects[request][]" value="'.$request.'" style="width:99%" /></td>
+						<td>&raquo;</td>
+						<td><input type="text" name="301_redirects[destination][]" value="'.$destination.'" style="width:99%;" /></td>
+						<td><span class="wps301-delete"></span></td>
 					</tr>
 					
 					';
@@ -90,9 +120,13 @@ if (!class_exists("Simple301redirects")) {
 			return $output;
 		}
 		
-		/*
-			save the redirects from the options page to the database
-		*/
+		/**
+		 * save_redirects function
+		 * save the redirects from the options page to the database
+		 * @access public
+		 * @param mixed $data
+		 * @return void
+		 */
 		function save_redirects($data) {
 			$redirects = array();
 			
@@ -104,14 +138,23 @@ if (!class_exists("Simple301redirects")) {
 				else { $redirects[$request] = $destination; }
 			}
 			
-			$redirects = array_reverse($redirects);
 			update_option('301_redirects', $redirects);
+			
+			if (isset($data['wildcard'])) {
+				update_option('301_redirects_wildcard', 'true');
+			}
+			else {
+				delete_option('301_redirects_wildcard');
+			}
 		}
 		
-		/*
-			Read the list of redirects and if the current page 
-			is found in the list, send the visitor on her way
-		*/
+		/**
+		 * redirect function
+		 * Read the list of redirects and if the current page 
+		 * is found in the list, send the visitor on her way
+		 * @access public
+		 * @return void
+		 */
 		function redirect() {
 			// this is what the user asked for (strip out home portion, case insensitive)
 			$userrequest = str_ireplace(get_option('home'),'',$this->get_address());
@@ -119,15 +162,39 @@ if (!class_exists("Simple301redirects")) {
 			
 			$redirects = get_option('301_redirects');
 			if (!empty($redirects)) {
+				
+				$wildcard = get_option('301_redirects_wildcard');
+				$do_redirect = '';
+				
+				// compare user request to each 301 stored in the db
 				foreach ($redirects as $storedrequest => $destination) {
-					// compare user request to each 301 stored in the db
-					if(urldecode($userrequest) == rtrim($storedrequest,'/')) {
+					// check if we should use regex search 
+					if ($wildcard === 'true' && strpos($storedrequest,'*') !== false) {
+						// wildcard redirect
+						
+						// Make sure it gets all the proper decoding and rtrim action
+						$storedrequest = str_replace('*','(.*)',$storedrequest);
+						$pattern = '/' . str_replace( '/', '\/', rtrim( $storedrequest, '/' ) ) . '/';
+						$destination = str_replace('*','$1',$destination);
+						$output = preg_replace($pattern, $destination, $userrequest);
+						if ($output !== $userrequest) {
+							// pattern matched, perform redirect 
+							$do_redirect = $output;
+						}
+					}
+					elseif(urldecode($userrequest) == rtrim($storedrequest,'/')) {
+						// simple comparison redirect
+						$do_redirect = $destination;
+					}
+					
+					if ($do_redirect !== '') {
 						// check if destination needs the domain prepended
-						if (strpos($destination,'/') === 0){
-							$destination = $this->get_protocol().'://'.$_SERVER['HTTP_HOST'].$destination;
+						if (strpos($do_redirect,'/') === 0){
+							// this seems to be broken, at least for wildcard searches. fix!
+							$do_redirect = $this->get_protocol().'://'.$_SERVER['HTTP_HOST'].$do_redirect;
 						}
 						header ('HTTP/1.1 301 Moved Permanently');
-						header ('Location: ' . $destination);
+						header ('Location: ' . $do_redirect);
 						exit();
 					}
 					else { unset($redirects); }
@@ -135,10 +202,13 @@ if (!class_exists("Simple301redirects")) {
 			}
 		} // end funcion redirect
 		
-		/*
-			utility function to get the full address of the current request
-			credit: http://www.phpro.org/examples/Get-Full-URL.html
-		*/
+		/**
+		 * getAddress function
+		 * utility function to get the full address of the current request
+		 * credit: http://www.phpro.org/examples/Get-Full-URL.html
+		 * @access public
+		 * @return void
+		 */
 		function get_address() {
 			// return the full address
 			return $this->get_protocol().'://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
